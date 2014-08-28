5c38013a5f18faa7cf7448d9cc32fb285209117e
Roberto <rober@null.net>
2014-08-28 09:12:18 -0500

    commit inicial

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+
+    /*
+     * 
+     * Requerimientos::
+     * 1. Concatenr 2 cadenas
+     * 2. Regresar el tamaño (length) de la cadaena.
+     * 3. ¿Contiene alguna letra?
+     * 4. ¿Contiene numeros?
+     * 
+     */
+    public static class Class1
+    {
+
+        //metodo para concatenar dos cadenas
+        public static string concatena(string s1,string s2 ) 
+        {
+
+            Console.WriteLine("Este es el resultado de las cadenas");
+
+
+
+
+
+
+            return s1 + s2;
+        }
+    }
+}
---
+++ Componente1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ClassLibrary1;
+
+namespace Componente1
+{
+    /*
+     *   aosndasjkndja
+     *   ajsndakjsndkj
+     *   aosdnajsndajksn
+     *   ajskndkasjnd
+     *   akjsndakjsdnakj
+     *   ajsndkajs
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Componente 1");
+
+            Console.WriteLine(Class1.concatena("hola ", "mundo"));
+
+            Console.Read();
+        }
+    }
+}
